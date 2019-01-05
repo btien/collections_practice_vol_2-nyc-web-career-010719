40e4cc9bddee0540650d15c8b3bd23ad134f7beb
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-05 02:15:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,10 @@
 def begins_with_r(array)
+  a = 0
   array.each do |word|
     if word.start_with? ("r")
-  
+      a += 1
+  if a == array.size
+      return true
+    else
+      return false
 end
